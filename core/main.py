161d29480091c55d7f0588f783591ddc631920a8
Grayback <antoine.pinon0110@gmail.com>
2018-12-02 19:45:30 +0100

    gitignore

--- core/main.py
+++ core/main.py
@@ -45,3 +45,4 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--verbose', help='verbose output', dest='verbose', action='store_true')
 args = parser.parse_args()
 
+print("bonjour")
